2b533ca5b5a1c494a03dbb31e53d1caca1818591
a-box31 <thomaa2031@gmail.com>
2024-07-24 00:29:20 -0400

    Backend + Frontend
    Creating the backend and frontend to the Ablog website.

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,24 @@ import express from "express";
 import { getUsers, getUser, createUser } from "./database.js";
 
 const app = express();
+app.use(express.json());
 
 app.get("/users", async (req, res) => {
     const users = await getUsers();
     res.send(users);
 });
+
+app.get("/users/:id", async (req, res) => {
+  const id = req.params.id;
+  const user = await getUser(id);
+  res.send(user);
+});
+
+app.post("/users", async (req, res) => {
+    const { username, password, email, gender } = req.body
+    const user = await createUser(username, password, email, gender);
+    res.status(201).send(user);
+})
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
