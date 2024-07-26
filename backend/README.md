3103f67401220241cd1d55b27e1bdbc8e7ce34e3
a-box31 <thomaa2031@gmail.com>
2024-07-26 15:37:16 -0400

    Register + Login Backend
    Created two endpoints where register
    1. hashes the password
    2. creates the user in the database
    3. sends back if the email already exists or success in creating the resource
    
    And login
    1. gets the user by the email
    2. sends back if the email was not found
    3. checks the password against the database password
    4. To Be Implemented - will create a session in the database for the user and return a session id in the form of a cookie. Otherwise send an incorrect password message.

--- backend/README.md
+++ backend/README.md
@@ -4,3 +4,7 @@ Creating a blogging website backend using mySQL, Express.js, and Node.js.
 
 # Status Codes
 # 201 - CREATED
+# 401 - CLIENT SIDE ERROR (lack of valid authentication credentials)
+# 404 - NOT FOUND
+# 403 - FORBIDDEN (already exists)
+# 500 - INTERNAL SERVER ERROR
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
+import { getUsers, getUserByID, getUserByEmail, createUser } from "./database.js";
+
 import express from "express";
 import cors from "cors";
-import { getUsers, getUser, createUser } from "./database.js";
+import bcrypt from "bcrypt";
 
 const app = express();
 
@@ -25,21 +27,56 @@ app.use(express.urlencoded({ extended: false }));
 //     res.status(201).send(user);
 // })
 
-app.post('/login', (req, res) => {
-  const { email, password } = req.body;
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await getUserByEmail(email);
+    // user not found
+    if (user == null) {
+      res.status(404).send("Email not found");
+      return;
+    }
+    // check if the password is correct
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    // if the password is correct, send the user object
+    if (passwordMatch) {
+      // create a new session for the user 
+
+      // return the to the cookie of the session to the client 
+      
+      res.status(200).send(user);
+    } else {
+      res.status(401).send("Incorrect password");
+    }
+
+  } catch (e) {
+    console.error(e);
+    // internal server error
+    res.sendStatus(500);
+  }
 
 });
 
-app.post('/register', async (req, res) => {
-  const {username, email, password} = req.body
-  const user = await createUser(username, password, email);
-  if(user != null) {
-    // user created successfully 
-    res.sendStatus(201);
-  }else{
-    // failed to create user
-    res.sendStatus(500);
+app.post("/register", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    // hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await createUser(username, hashedPassword, email);
+    if (user != null) {
+      // user created successfully
+      res.sendStatus(201);
+    } else {
+      // failed to create user / email already exists
+      res.status(403).send("Email already exists");
+    }
+  } catch (e) {
+    console.error(e);
+    // internal server error
+    res.status(500).send("Internal server error");  
   }
+
 });
 
 app.use((err, req, res, next) => {
