267bac355d2028afde2373aace02a43ae6a9e1ef
a-box31 <thomaa2031@gmail.com>
2024-07-28 14:31:11 -0400

    Session Check
    check if the user is logged in by a use effect hook that mounts a interval function which checks if the user has the cookie, sesssionID. If they do then then show them the updated UI for when a user is logged in.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,36 @@
-import { Routes, Route } from "react-router-dom"
-import { useState } from "react";
-import Layouts from "./components/Layouts"
-import Home from "./components/Home"
-import Login from "./components/Login"
-import Register from "./components/Register"
+import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Layouts from "./components/Layouts";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Account from "./components/Account";
 import "./App.scss";
+import Cookies from "js-cookie";
 
 
 const App = () => {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const checkSessionCookie = () => {
+    const sessionID = Cookies.get("sessionID");
+    if (sessionID) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }
+
+  useEffect(() => {
+    // initial check
+    checkSessionCookie();
+    // check every 5 seconds for the session cookie
+    const intervalID = setInterval(checkSessionCookie, 1000);
+    // cleanup
+    return () => clearInterval(intervalID);
+  }, []);
+
 
   return (
     <>
@@ -18,9 +39,9 @@ const App = () => {
           <Route index element={<Home />} />
           {/* <Route path="create" element={<Create />} /> */}
           {/* <Route path="contact" element={<Contact />} /> */}
-          <Route path="login" element={<Login />} />
+          <Route path="login" element={<Login isLoggedIn={isLoggedIn} />} />
           <Route path="register" element={<Register />} />
-          {/* <Route path="account" element={<Account />} /> */}
+          <Route path="account" element={<Account isLoggedIn={isLoggedIn} />} /> 
         </Route>
       </Routes>
     </>
