a39a4b3419af40e4bfce7f733f60e277a2acb2b8
a-box31 <thomaa2031@gmail.com>
2024-07-31 14:25:43 -0400

    Avatar on Tables
    store the name of the file on the database for the avatar of the user,

--- backend/ABlogDB.session.sql
+++ backend/ABlogDB.session.sql
@@ -2,7 +2,8 @@ CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE
+    email VARCHAR(255) NOT NULL UNIQUE,
+    avatar VARCHAR(255) DEFAULT 'default.png'
 );
 
 -- @block
