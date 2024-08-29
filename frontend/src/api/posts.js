a1204470f4f07f8c3c37c9e197095c1c1886cff8
a-box31 <thomaa2031@gmail.com>
2024-08-29 12:45:43 -0400

    environment variables

--- frontend/src/api/posts.js
+++ frontend/src/api/posts.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
-    withCredentials: true,
+  baseURL: import.meta.env.VITE_APP_API_URL,
+  withCredentials: true,
 });
