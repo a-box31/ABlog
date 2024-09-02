7a1b3e2f77ad31f6016ffdcb7bad21e149ef782b
kiinshuk <red2k2green@gmail.com>
2024-09-02 13:27:06 +0530

    sidebar styling

--- frontend/src/components/SideBar/index.jsx
+++ frontend/src/components/SideBar/index.jsx
@@ -3,13 +3,33 @@ import "./index.scss";
 const SideBar = () => {
     return (
         <div className="sidebar">
-            <h2>SideBar</h2>
-            <div className="following-container">
-                <h3>Following</h3>
+            <div className="section">
                 <ul>
-                    <li>Following 1</li>
-                    <li>Following 2</li>
-                    <li>Following 3</li>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/feed">Main Feed</a></li>
+                    <li><a href="/followers">Followers Feed</a></li>
+                    <li><a href="/following">Following</a></li>
+                </ul>
+            </div>
+            <div className="section">
+                <h3>Popular</h3>
+                <ul>
+                    <li>Trending</li>
+                    <li>Top Rated</li>
+                    <li>New Releases</li>
+                </ul>
+            </div>
+            <div className="section">
+                <h3>My Network</h3>
+                <ul>
+                    <li>My Followers</li>
+                    <li>Following</li>
+                    <li>Suggestions</li>
+                </ul>
+            </div>
+            <div className="settings-section">
+                <ul>
+                    <li><a href="/settings">Settings</a></li>
                 </ul>
             </div>
         </div>
