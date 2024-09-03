12b68d0bd6f9c89df68b98981abdff3f306e4f2d
a-box31 <thomaa2031@gmail.com>
2024-09-03 15:47:16 -0400

    Getting Rid of Top Nav and Footer for a Sidebar

--- frontend/src/components/Layouts/index.jsx
+++ frontend/src/components/Layouts/index.jsx
@@ -1,17 +1,13 @@
 import "./index.scss";
 import { Outlet } from "react-router-dom";
 import Sidebar from "../SideBar";
-import NavBar from "../NavBar";
-import Footer from "../Footer";
 
 const Layouts = () => {
     return (
       <div className="overlay">
-        {/* <NavBar /> */}
         <Sidebar />
         <div className="page">
           <Outlet />
-          {/* <Footer /> */}
         </div>
       </div>
     );
