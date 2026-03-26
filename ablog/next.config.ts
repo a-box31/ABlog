b762c51125e93ce8cb0cf3d66c95176da19ae6d3
a-box31 <thomaa2031@gmail.com>
2026-03-26 16:03:50 -0400

    Created Drizzle Connection and instantiated the Docker file for containerization

--- ablog/next.config.ts
+++ ablog/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  sassOptions: {
+    implementation: 'sass-embedded',
+  },
 };
 
 export default nextConfig;
